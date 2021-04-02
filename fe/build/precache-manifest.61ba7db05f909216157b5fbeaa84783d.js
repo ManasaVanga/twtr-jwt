90845dd391763e90e19f35c9ffc3130bf6f843d1
ManasaVanga <vanga.m@northeastern.edu>
2021-04-02 16:33:06 -0400

    removed build from gitignore

---
+++ fe/build/precache-manifest.61ba7db05f909216157b5fbeaa84783d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6140f59079ed109f3068248d11e21a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533d336b230362786499",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "70b80edd643775e66359",
+    "url": "/static/js/2.4eb74f49.chunk.js"
+  },
+  {
+    "revision": "9a5d22d7b0e21afebd28f4768943f0bf",
+    "url": "/static/js/2.4eb74f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533d336b230362786499",
+    "url": "/static/js/main.d034895c.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
