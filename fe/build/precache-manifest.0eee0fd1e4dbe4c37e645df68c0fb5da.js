2f1d2524e5de9be62b18f07382612a76fc6e00b0
ManasaVanga <vanga.m@northeastern.edu>
2021-04-02 18:30:53 -0400

    hardcoding api url

--- fe/build/precache-manifest.0eee0fd1e4dbe4c37e645df68c0fb5da.js
+++ fe/build/precache-manifest.0eee0fd1e4dbe4c37e645df68c0fb5da.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb6140f59079ed109f3068248d11e21a",
+    "revision": "04b6e8707e133b0d833e80011d18c75c",
     "url": "/index.html"
   },
   {
-    "revision": "533d336b230362786499",
+    "revision": "e612121406be15736dfd",
     "url": "/static/css/main.7f904051.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.4eb74f49.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "533d336b230362786499",
-    "url": "/static/js/main.d034895c.chunk.js"
+    "revision": "e612121406be15736dfd",
+    "url": "/static/js/main.625b311f.chunk.js"
   },
   {
     "revision": "07ef167960929c3ab583",
--- fe/src/pages/SignIn/SignIn.js
+++ fe/src/pages/SignIn/SignIn.js
@@ -126,7 +126,7 @@ const SignIn = () => {
     // verify user/pwd, get encoded userid as access and refresh tokens in return
     //fetch("http://localhost:5000/login", config)
     //fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/login`, config)
-    fetch(`${process.env.REACT_APP_API_SERVICE_URL}/login`, config)
+    fetch(`http://flask-react-alb-1099811801.us-east-1.elb.amazonaws.com/login`, config)
       .then(response => response.json())
       .then(data => {
 
